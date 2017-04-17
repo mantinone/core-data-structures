1e93ba43f7061b4a9a6599327b5260e3f164ef50
Max Antinone <mantinone@gmail.com>
2017-04-17 15:51:23 -0700

    Implements stack with node

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,43 @@
 'use strict'
 
+import Node from '../src/node'
+
 export default class Stack {
-  // your code here
+  constructor() {
+    this.top = null
+    this.size = 0
+  }
+
+  push( data ) {
+    let newNode = new Node( data, this.top )
+    this.top = newNode
+    this.size++
+  }
+
+  pop() {
+    let result = this.top.data
+    this.top = this.top.next
+    this.size--
+    return result
+  }
+
+  peek() {
+    if( this.isEmpty() ) {
+      return null
+    } else {
+      return this.top.data
+    }
+  }
+
+  isEmpty() {
+    if ( this.length() > 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  length() {
+    return this.size
+  }
 }
